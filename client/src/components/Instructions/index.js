af7bbf89f168dbdd608673ddb2991649d0a2111f
l3a0 <bao@baowebdev.com>
2019-12-01 21:08:57 -0800

    currency conversion.

--- client/src/components/Instructions/index.js
+++ client/src/components/Instructions/index.js
@@ -15,10 +15,13 @@ export default class Instructions extends Component {
     switch (name) {
       case 'unhealthyAccounts':
         await this.loadUnhealthyAccounts();
+        break;
       case 'liquidations':
         await this.loadLiquidations();
+        break;
       default:
-        return this.loadUnhealthyAccounts();
+        this.loadUnhealthyAccounts();
+        break;
     }
   }
   
@@ -36,20 +39,22 @@ export default class Instructions extends Component {
         // In theory options parameter is optional. In practice an empty array is
         // returned if options is not provided with fromBlock and toBlock set.
         let liquidations = await compoundUsd.getPastEvents('LiquidateBorrow', options);
-        const totalLiquidation = this.calculateTotalLiquidation(liquidations);
+        // Prices are in USD ($).
         const addressToSymbolMap = {
-          '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5': 'cETH',
-          '0x158079Ee67Fce2f58472A96584A73C7Ab9AC95c1': 'cREP',
-          '0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E': 'cBAT',
-          '0xF5DCe57282A584D2746FaF1593d3121Fcac444dC': 'cSAI',
-          '0xB3319f5D18Bc0D84dD1b4825Dcde5d5f7266d407': 'cZRX',
-          '0xC11b1268C1A384e55C48c2391d8d480264A3A7F4': 'cWBTC',
-          '0xC11b1268C1A384e55C48c2391d8d480264A3A7F4': 'cWBTC',
+          '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5': {'symbol': 'cETH', 'price': 3.0154},
+          '0x158079Ee67Fce2f58472A96584A73C7Ab9AC95c1': {'symbol': 'cREP', 'price': 0.2126},
+          '0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E': {'symbol': 'cBAT', 'price': 0.0039},
+          '0xF5DCe57282A584D2746FaF1593d3121Fcac444dC': {'symbol': 'cSAI', 'price': 0.0211},
+          '0xB3319f5D18Bc0D84dD1b4825Dcde5d5f7266d407': {'symbol': 'cZRX', 'price': 0.0051},
+          '0xC11b1268C1A384e55C48c2391d8d480264A3A7F4': {'symbol': 'cWBTC', 'price': 148.3473},
         }
         liquidations.forEach(element => {
-          element.symbol = addressToSymbolMap[element.returnValues['cTokenCollateral']];
+          element.symbol = addressToSymbolMap[element.returnValues['cTokenCollateral']].symbol;
+          element.revenue = addressToSymbolMap[element.returnValues['cTokenCollateral']].price * (element.returnValues['seizeTokens'] / 10**8);
         });
-        this.setState({ liquidations: liquidations, totalLiquidation: totalLiquidation });
+        const totalLiquidation = this.calculateTotalLiquidation(liquidations);
+        const totalRevenue = this.calculateTotalRevenue(liquidations);
+        this.setState({ liquidations: liquidations, totalLiquidation: totalLiquidation, totalRevenue: totalRevenue });
       }
       catch (error) {
         console.log(error);
@@ -62,8 +67,14 @@ export default class Instructions extends Component {
       let repayAmount = (currentValue.returnValues['repayAmount'] / 10 ** 6);
       return accumulator + repayAmount;
     };
-    const totalRevenue = liquidations.reduce(sum, 0);
-    return totalRevenue;
+    return liquidations.reduce(sum, 0);
+  }
+
+  calculateTotalRevenue(liquidations) {
+    const sum = (accumulator, currentValue) => {
+      return accumulator + currentValue.revenue;
+    };
+    return liquidations.reduce(sum, 0);
   }
 
   async loadUnhealthyAccounts() {
@@ -136,6 +147,7 @@ export default class Instructions extends Component {
                 <th>Liquidator</th>
                 <th>Borrower</th>
                 <th>Repay Amount (USDC)</th>
+                <th>Revenue ($)</th>
                 <th>Revenue (cToken)</th>
                 <th>cToken</th>
               </tr>
@@ -146,7 +158,7 @@ export default class Instructions extends Component {
                 <td></td>
                 <td></td>
                 <td><NumberFormat value={this.state.totalLiquidation} displayType={'text'} thousandSeparator={true} /></td>
-                <td></td>
+                <td><NumberFormat value={this.state.totalRevenue} displayType={'text'} thousandSeparator={true} /></td>
                 <td></td>
               </tr>
               {this.state.liquidations.map((value, index) => {
@@ -156,6 +168,7 @@ export default class Instructions extends Component {
                     <td>{value.returnValues['liquidator']}</td>
                     <td>{value.returnValues['borrower']}</td>
                     <td><NumberFormat value={value.returnValues['repayAmount'] / 10**6} displayType={'text'} thousandSeparator={true} /></td>
+                    <td><NumberFormat value={value.revenue} displayType={'text'} thousandSeparator={true} /></td>
                     <td><NumberFormat value={value.returnValues['seizeTokens'] / 10**8} displayType={'text'} thousandSeparator={true} /></td>
                     <td>{value.symbol}</td>
                   </tr>
