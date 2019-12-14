86f74baa63aade507c552fcd41e5f173496e9a12
l3a0 <bao@baowebdev.com>
2019-12-14 14:53:54 -0800

    calculate expected transaction.

--- client/src/components/UnhealthyAccounts/index.js
+++ client/src/components/UnhealthyAccounts/index.js
@@ -32,37 +32,49 @@ export default class UnhealthyAccounts extends Component {
         '0xb3319f5d18bc0d84dd1b4825dcde5d5f7266d407': {'symbol': 'cZRX', 'price': 0.0051, 'underlyingAssetToEthExchangeRate': 0.001524},
         '0xc11b1268c1a384e55c48c2391d8d480264a3a7f4': {'symbol': 'cWBTC', 'price': 148.3473, 'underlyingAssetToEthExchangeRate': 50.753489},
       };
+      const showUsd = true;
+      const ethToUsd = 143.77;
       data.accounts.forEach(account => {
         account.debt = [];
         account.collateral = [];
-        account.max_liquidation_value_in_eth = parseFloat(account.total_borrow_value_in_eth.value) * data.close_factor;
+        account.total_borrow_value_in_eth = parseFloat(account.total_borrow_value_in_eth.value);
+        account.total_borrow_value_in_usd = account.total_borrow_value_in_eth * ethToUsd;
+        account.max_liquidation_value_in_eth = account.total_borrow_value_in_eth * data.close_factor;
+        account.max_liquidation_value_in_usd = account.max_liquidation_value_in_eth * ethToUsd;
+        account.total_collateral_value_in_eth = parseFloat(account.total_collateral_value_in_eth.value);
+        account.total_collateral_value_in_usd = account.total_collateral_value_in_eth * ethToUsd;
         account.total_debt_in_eth = 0;
         account.total_supply_in_eth = 0;
         account.tokens.forEach(token => {
           token.symbol = addressToSymbolMap[token.address].symbol;
           token.price = addressToSymbolMap[token.address].price;
           token.underlyingAssetToEthExchangeRate = addressToSymbolMap[token.address].underlyingAssetToEthExchangeRate;
-          token.borrow_balance_underlying_in_eth = parseFloat(token.borrow_balance_underlying.value) * addressToSymbolMap[token.address].underlyingAssetToEthExchangeRate;
+          token.borrow_balance_underlying = parseFloat(token.borrow_balance_underlying.value);
+          token.borrow_balance_underlying_in_eth = token.borrow_balance_underlying * addressToSymbolMap[token.address].underlyingAssetToEthExchangeRate;
+          token.borrow_balance_underlying_in_usd = token.borrow_balance_underlying_in_eth * ethToUsd;
           account.total_debt_in_eth += token.borrow_balance_underlying_in_eth;
-          token.supply_balance_underlying_in_eth = parseFloat(token.supply_balance_underlying.value) * addressToSymbolMap[token.address].underlyingAssetToEthExchangeRate;
+          token.supply_balance_underlying = parseFloat(token.supply_balance_underlying.value);
+          token.supply_balance_underlying_in_eth = token.supply_balance_underlying * addressToSymbolMap[token.address].underlyingAssetToEthExchangeRate;
+          token.supply_balance_underlying_in_usd = token.supply_balance_underlying_in_eth * ethToUsd;
           account.total_supply_in_eth += token.supply_balance_underlying_in_eth;
-          const borrowBalance = parseFloat(token.borrow_balance_underlying.value);
-          if (borrowBalance > 0) {
+          if (token.borrow_balance_underlying > 0) {
             account.debt.push(token);
           }
-          const supplyBalance = parseFloat(token.supply_balance_underlying.value);
-          if (supplyBalance > 0) {
+          if (token.supply_balance_underlying > 0) {
             account.collateral.push(token);
           }
         });
+        account.total_debt_in_usd = account.total_debt_in_eth * ethToUsd;
+        account.total_supply_in_usd = account.total_supply_in_eth * ethToUsd;
         account.debt.sort((a, b) => b.borrow_balance_underlying_in_eth - a.borrow_balance_underlying_in_eth);
         account.collateral.sort((a, b) => b.supply_balance_underlying_in_eth - a.supply_balance_underlying_in_eth);
       });
-      data.accounts.sort((a, b) => parseFloat(b.total_borrow_value_in_eth.value) - parseFloat(a.total_borrow_value_in_eth.value));
+      data.accounts.sort((a, b) => b.total_borrow_value_in_eth - a.total_borrow_value_in_eth);
       this.setState({
         accountResponse: data,
-        ethToUsd: 143.77,
-        showUsd: false,
+        ethToUsd: ethToUsd,
+        showUsd: showUsd,
+        currencySymbol: showUsd ? '$' : 'Ξ',
       });
       console.log(data);
     }
@@ -82,14 +94,14 @@ export default class UnhealthyAccounts extends Component {
               <tr>
                 <th>Address</th>
                 <th align="right">Health</th>
-                <th align="right">Borrow Value ({this.state.showUsd ? '$' : 'Ξ'})</th>
-                <th align="right">Max Liquidation Amount ({this.state.showUsd ? '$' : 'Ξ'})</th>
+                <th align="right">Borrow Value ({this.state.currencySymbol})</th>
+                <th align="right">Max Liquidation Amount ({this.state.currencySymbol})</th>
                 <th align="right">Debt</th>
-                <th align="right">Debt ({this.state.showUsd ? '$' : 'Ξ'})</th>
-                <th align="right">Collateral Value ({this.state.showUsd ? '$' : 'Ξ'})</th>
+                <th align="right">Debt ({this.state.currencySymbol})</th>
+                <th align="right">Collateral Value ({this.state.currencySymbol})</th>
                 <th align="right">Collateral</th>
-                <th align="right">Collateral ({this.state.showUsd ? '$' : 'Ξ'})</th>
-                <th>Transaction</th>
+                <th align="right">Collateral ({this.state.currencySymbol})</th>
+                <th>Expected Transaction</th>
               </tr>
             </thead>
             <tbody>
@@ -98,14 +110,13 @@ export default class UnhealthyAccounts extends Component {
                   <tr key={account.address}>
                     <td>{account.address}</td>
                     <td align="right">{parseFloat(account.health.value).toFixed(8)}</td>
-                    <td align="right">{this.state.showUsd ? (parseFloat(account.total_borrow_value_in_eth.value) * this.state.ethToUsd).toFixed(8) : parseFloat(account.total_borrow_value_in_eth.value).toFixed(8)}</td>
-                    <td align="right">{this.state.showUsd ? (account.max_liquidation_value_in_eth * this.state.ethToUsd).toFixed(8) : account.max_liquidation_value_in_eth.toFixed(8)}</td>
+                    <td align="right">{this.state.showUsd ? account.total_borrow_value_in_usd.toFixed(8) : account.total_borrow_value_in_eth.toFixed(8)}</td>
+                    <td align="right">{this.state.showUsd ? account.max_liquidation_value_in_usd.toFixed(8) : account.max_liquidation_value_in_eth.toFixed(8)}</td>
                     <td align="right">
                       <ul>
                         {(account.debt.map((token) => {
-                          const debt = parseFloat(token.borrow_balance_underlying.value).toFixed(8);
                           return (
-                            <li key={token.address}>{token.symbol.substring(1) + ": " + debt}</li>
+                            <li key={token.address}>{token.symbol.substring(1) + ": " + token.borrow_balance_underlying.toFixed(8)}</li>
                           );
                         }))}
                       </ul>
@@ -113,21 +124,19 @@ export default class UnhealthyAccounts extends Component {
                     <td align="right">
                       <ul>
                         {(account.debt.map((token) => {
-                          const debt = this.state.showUsd ? (parseFloat(token.borrow_balance_underlying_in_eth) * this.state.ethToUsd).toFixed(8) : parseFloat(token.borrow_balance_underlying_in_eth).toFixed(8);
                           return (
-                            <li key={token.address}>{token.symbol.substring(1) + ": " + debt}</li>
+                            <li key={token.address}>{token.symbol.substring(1)}: {this.state.showUsd ? token.borrow_balance_underlying_in_usd.toFixed(8) : token.borrow_balance_underlying_in_eth.toFixed(8)}</li>
                           );
                         }))}
-                        <li>Total: {this.state.showUsd ? (account.total_debt_in_eth * this.state.ethToUsd).toFixed(8) : account.total_debt_in_eth.toFixed(8)}</li>
+                        <li>Total: {this.state.showUsd ? account.total_debt_in_usd.toFixed(8) : account.total_debt_in_eth.toFixed(8)}</li>
                       </ul>
                     </td>
-                    <td align="right">{(parseFloat(account.total_collateral_value_in_eth.value) * this.state.ethToUsd).toFixed(8)}</td>
+                    <td align="right">{(this.state.showUsd ? account.total_collateral_value_in_usd : account.total_collateral_value_in_eth).toFixed(8)}</td>
                     <td align="right">
                       <ul>
                         {(account.collateral.map((token) => {
-                          const collateral = parseFloat(token.supply_balance_underlying.value).toFixed(8);
                           return (
-                            <li key={token.address}>{token.symbol.substring(1) + ": " + collateral}</li>
+                            <li key={token.address}>{token.symbol.substring(1) + ": " + token.supply_balance_underlying.toFixed(8)}</li>
                           );
                         }))}
                       </ul>
@@ -135,16 +144,18 @@ export default class UnhealthyAccounts extends Component {
                     <td align="right">
                       <ul>
                         {(account.collateral.map((token) => {
-                          const collateral = this.state.showUsd ? (parseFloat(token.supply_balance_underlying_in_eth) * this.state.ethToUsd).toFixed(8) : parseFloat(token.supply_balance_underlying_in_eth).toFixed(8);
                           return (
-                            <li key={token.address}>{token.symbol.substring(1) + ": " + collateral}</li>
+                            <li key={token.address}>{token.symbol.substring(1)}: {this.state.showUsd ? token.supply_balance_underlying_in_usd.toFixed(8) : token.supply_balance_underlying_in_eth.toFixed(8)}</li>
                           );
                         }))}
-                        <li>Total: {this.state.showUsd ? (account.total_supply_in_eth * this.state.ethToUsd).toFixed(8) : account.total_supply_in_eth.toFixed(8)}</li>
+                        <li>Total: {this.state.showUsd ? account.total_supply_in_usd.toFixed(8) : account.total_supply_in_eth.toFixed(8)}</li>
                       </ul>
                     </td>
                     <td>
-                      Liquidate {(account.max_liquidation_value_in_eth / account.debt[0].underlyingAssetToEthExchangeRate).toFixed(8)} {account.debt[0].symbol.substring(1)} for {(account.max_liquidation_value_in_eth > account.collateral[0].supply_balance_underlying_in_eth ? parseFloat(account.collateral[0].supply_balance_underlying.value) : account.max_liquidation_value_in_eth / account.collateral[0].underlyingAssetToEthExchangeRate).toFixed(8)} {account.collateral[0].symbol.substring(1)}
+                      <ul>
+                        <li>Liquidate {(account.max_liquidation_value_in_eth / account.debt[0].underlyingAssetToEthExchangeRate).toFixed(8)} {account.debt[0].symbol.substring(1)} debt</li>
+                        <li>Collect {(account.max_liquidation_value_in_eth > account.collateral[0].supply_balance_underlying_in_eth ? account.collateral[0].supply_balance_underlying : account.max_liquidation_value_in_eth * this.state.accountResponse.liquidation_incentive / account.collateral[0].underlyingAssetToEthExchangeRate).toFixed(8)} {account.collateral[0].symbol.substring(1)} collateral</li>
+                      </ul>
                     </td>
                   </tr>
                 );
