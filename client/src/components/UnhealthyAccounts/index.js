0278fa7eaca5d2cc196484d68cd3952e8503de33
l3a0 <bao@baowebdev.com>
2019-12-15 17:26:34 -0800

    calculate expected profit

--- client/src/components/UnhealthyAccounts/index.js
+++ client/src/components/UnhealthyAccounts/index.js
@@ -13,10 +13,12 @@ import CWBTCABI from '../../../../contracts/cwbtc.json';
 export default class UnhealthyAccounts extends Component {
   state = {
     showUsd: true,
+    // TODO: Get price from oracle.
     ethToUsd: 143.77,
   };
   // Prices are in USD ($).
   // TODO: Put this in IndexedDB.
+  // TODO: Get prices from oracles.
   tokenMap = {
     '0x39aa39c021dfbae8fac545936693ac917d5e7563': {
       symbol: 'cUSDC',
@@ -80,9 +82,10 @@ export default class UnhealthyAccounts extends Component {
     const web3 = await getWeb3();
     this.setState({
       web3: web3,
+      // Fast option on ETH Gas Station.
       gasPrice: new web3.utils.BN(13.2 * 10**9),
+      currencySymbol: this.state.showUsd ? '$' : 'Ξ',
     });
-    // console.log(this.state.gasPrice.toString());
     await this.loadTokenContracts();
     await this.loadUnhealthyAccounts();
   }
@@ -153,8 +156,8 @@ export default class UnhealthyAccounts extends Component {
         }
         const liquidationAmount = account.max_liquidation_value_in_eth / account.debt[0].underlyingAssetToEthExchangeRate;
         const seizeAmount = account.max_liquidation_value_in_eth * data.liquidation_incentive / account.collateral[0].underlyingAssetToEthExchangeRate;
-        account.transactions.push(`Liquidate ${liquidationAmount.toFixed(8)} ${account.debt[0].symbol.substring(1)}, $${account.max_liquidation_value_in_usd.toFixed(8)} debt`)
-        account.transactions.push(`Collect ${seizeAmount.toFixed(8)} ${account.collateral[0].symbol.substring(1)}, $${(account.max_liquidation_value_in_usd * data.liquidation_incentive).toFixed(8)} collateral`)
+        account.transactions.push(`Liquidate ${liquidationAmount.toFixed(8)} ${account.debt[0].symbol.substring(1)}, ${this.state.currencySymbol}${(account.max_liquidation_value_in_eth * (this.state.showUsd ? this.state.ethToUsd : 1)).toFixed(8)} debt`)
+        account.transactions.push(`Collect ${seizeAmount.toFixed(8)} ${account.collateral[0].symbol.substring(1)}, ${this.state.currencySymbol}${(account.max_liquidation_value_in_eth * (this.state.showUsd ? this.state.ethToUsd : 1) * data.liquidation_incentive).toFixed(8)} collateral`)
         let expectedGasAmount = 0;
         if (account.debt[0].symbol === 'cETH') {
           expectedGasAmount = await account.debt[0].contract.methods.liquidateBorrow(account.address, account.collateral[0].address).estimateGas({gas: 5000000000, value: (liquidationAmount * 10**account.debt[0].decimals).toFixed(0)});
@@ -162,12 +165,14 @@ export default class UnhealthyAccounts extends Component {
           expectedGasAmount = await account.debt[0].contract.methods.liquidateBorrow(account.address, (liquidationAmount * 10**account.debt[0].decimals).toFixed(0), account.collateral[0].address).estimateGas({gas: 5000000000});
         }
         const expectedGasFee = this.state.web3.utils.fromWei(this.state.gasPrice.mul(this.state.web3.utils.toBN(expectedGasAmount)));
-        // console.log(`expectedGasFee = ${this.state.web3.utils.fromWei(expectedGasFee) * this.state.ethToUsd}`);
-        account.transactions.push(`Gas Amount = ${expectedGasAmount}, Gas Fee = $${(expectedGasFee * this.state.ethToUsd).toFixed(8)}`);
+        account.transactions.push(`Gas Amount = ${expectedGasAmount}, Gas Fee = ${this.state.currencySymbol}${(expectedGasFee * (this.state.showUsd ? this.state.ethToUsd : 1)).toFixed(8)}`);
+        const expectedRevenue = (seizeAmount * account.collateral[0].underlyingAssetToEthExchangeRate) - (liquidationAmount * account.debt[0].underlyingAssetToEthExchangeRate);
+        account.transactions.push(`Expected Revenue = ${this.state.currencySymbol}${(expectedRevenue * (this.state.showUsd ? this.state.ethToUsd : 1)).toFixed(8)}`);
+        const expectedProfit = expectedRevenue - expectedGasFee;
+        account.transactions.push(`Expected Profit = ${this.state.currencySymbol}${(expectedProfit * (this.state.showUsd ? this.state.ethToUsd : 1)).toFixed(8)}`);
       }
       this.setState({
         accountResponse: data,
-        currencySymbol: this.state.showUsd ? '$' : 'Ξ',
       });
     }
     catch (e) {
